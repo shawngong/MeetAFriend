a03c245e1aa9c33a6486328928f465daa02cbc52
Shawn Chen Gong <sc2gong@uwaterloo.ca>
2016-08-23 17:12:25 -0400

    servering up some simple static info

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 require('./app/index');
+const path = require('path');
 const express = require('express');
+const exphbs = require('express-handlebars');
 const app = express();
+
+app.engine('.hbs', exphbs({
+  defaultLayout: 'main',
+  extname: '.hbs',
+  layoutsDir: path.join(__dirname, 'views/layouts')
+}))
+app.set('view engine', '.hbs')
+app.set('views', path.join(__dirname, 'views'))
 
 app.use((request, response, next) => {
   console.log(request.headers)
@@ -12,9 +22,15 @@ app.use((request, response, next) => {
   next()
 })
 
-app.get('/', (request, response) => {
+app.get('/get', (request, response) => {
   response.json({
     chance: request.chance
+  })
+})
+
+app.get('/', (request, response) => {
+  response.render('home', {
+    name: 'John'
   })
 })
 
