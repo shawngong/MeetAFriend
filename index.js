e0ba10dadeea2251fddba6c392d0b00686180a42
Shawn Chen Gong <sc2gong@uwaterloo.ca>
2016-08-23 14:23:25 -0400

    added some error middleware and some basic routes for node.js backend with express

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
 require('./app/index');
+const express = require('express');
+const app = express();
+
+app.use((request, response, next) => {
+  console.log(request.headers)
+  next()
+})
+
+app.use((request, response, next) => {
+  request.chance = Math.random()
+  next()
+})
+
+app.get('/', (request, response) => {
+  response.json({
+    chance: request.chance
+  })
+})
+
+app.get('/error', (request, response) => {
+  throw new Error('oops')
+})
+
+app.use((err, request, response, next) => {
+  // log the error, for now just console.log
+  console.log(err)
+  response.status(500).send('Something broke!')
+})
+
+app.listen(3000);
 
 const _ = require('lodash');
 
