457a26d5e5bed5f871a5e0dac986e3bf396397ea
shawn <shawngong1996@hotmail.com>
2016-08-30 20:40:13 -0700

    added a homepage (simple template)

--- index.js
+++ index.js
@@ -5,54 +5,20 @@ const exphbs = require('express-handlebars');
 const request = require('request-promise');
 const passport = require('passport')
 const session = require('express-session')
-const RedisStore = require('connect-redis')(session)
 const config = require('./config')
 
 const app = express()
 
-app.use(session({
-  store: new RedisStore({
-    url: config.redisStore.url
-  }),
-  secret: config.redisStore.secret,
-  resave: false,
-  saveUninitialized: false
-}))
-app.use(passport.initialize())
-app.use(passport.session())
+app.listen(3000, function(){
+ console.log((new Date()) + ' Server is listening on port 3000');
+});
 
-
-app.get('/:city', (req, res) => {
-  rp({
-    uri: 'http://apidev.accuweather.com/locations/v1/search',
-    qs: {
-      q: req.params.city,
-      apiKey: 'api-key'
-         // Use your accuweather API key here
-    },
-    json: true
-  })
-    .then((data) => {
-      res.render('index', data)
-    })
-    .catch((err) => {
-      console.log(err)
-      res.render('error')
-    })
-})
-
-app.engine('.hbs', exphbs({
-  defaultLayout: 'main',
-  extname: '.hbs',
-  layoutsDir: path.join(__dirname, 'views/layouts')
-}))
-app.set('view engine', '.hbs')
-app.set('views', path.join(__dirname, 'views'))
+app.use(express.static(__dirname + 'views'));
 
 app.use((request, response, next) => {
   console.log(request.headers)
   next()
-})
+});
 
 app.use((request, response, next) => {
   request.chance = Math.random()
@@ -66,9 +32,7 @@ app.get('/get', (request, response) => {
 })
 
 app.get('/', (request, response) => {
-  response.render('home', {
-    name: 'John'
-  })
+  response.sendFile((path.join(__dirname + '/views/index.html')));
 })
 
 app.get('/error', (request, response) => {
@@ -81,13 +45,11 @@ app.use((err, request, response, next) => {
   response.status(500).send('Something broke!')
 })
 
-app.listen(3000);
+
 
 const _ = require('lodash');
 
 const fs = require('fs');
-
-_.assign({ 'a': 1 }, { 'b': 2 }, { 'c': 3 });
 
 function stats (file) {
   return new Promise((resolve, reject) => {
